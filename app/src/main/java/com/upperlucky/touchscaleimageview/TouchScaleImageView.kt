c0c9d03e0d0632c8fd2bb2485b3f6282746d0cbc
yunkun.wen <upperlucky@gmail.com>
2020-09-10 17:54:12 +0800

    Code optimization and add ScaleGestureDetector

--- app/src/main/java/com/upperlucky/touchscaleimageview/TouchScaleImageView.kt
+++ app/src/main/java/com/upperlucky/touchscaleimageview/TouchScaleImageView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import android.view.View
 import android.widget.OverScroller
 import androidx.core.view.GestureDetectorCompat
@@ -23,30 +24,31 @@ private val IMAGE_SIZE = 300.dp
 private const val EXTRA_SCALE_FACOTR = 1.5f
 private val OVER_SCROLL = 30.dp
 
-class TouchScaleImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs),
-    GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, Runnable {
+class TouchScaleImageView(context: Context?, attrs: AttributeSet?) : View(context, attrs){
 
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val bitmap = getAvatar(resources, IMAGE_SIZE.toInt())
     private var smallScale = 0f
     private var bigScale = 0f
-    private var currentScale = 0f
     private var originalOffsetX = 0f
     private var originalOffsetY = 0f
     private var offsetX = 0f
     private var offsetY = 0f
     private var big = false
-    private var scaleFraction = 0f
+    private val gestureListener = GestureListener()
+    private val scaleGestureListener = ScaleGestureListener()
+    private val flingRunnable = FlingRunnable()
+    private var currentScale = 0f
         set(value) {
             field = value
             invalidate()
         }
 
-    private var scaleAnimator = ObjectAnimator.ofFloat(this,"scaleFraction",0f,1f)
+    private var scaleAnimator = ObjectAnimator.ofFloat(this,"currentScale",0f,1f)
 
-    private val gestureDecoder = GestureDetectorCompat(context, this).apply {
-        setOnDoubleTapListener(this@TouchScaleImageView)
-    }
+
+    private val gestureDecoder = GestureDetectorCompat(context, gestureListener)
+    private val scaleGestureDetector = ScaleGestureDetector(context,scaleGestureListener)
 
     private val scroll = OverScroller(context)
 
@@ -68,92 +70,119 @@ class TouchScaleImageView(context: Context?, attrs: AttributeSet?) : View(contex
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        canvas.translate(offsetX,offsetY)
-        val scale = smallScale + (bigScale - smallScale) * scaleFraction
-        canvas.scale(scale, scale, width / 2f, height / 2f)
+        val scaleFraction = (currentScale - smallScale) / (bigScale - smallScale)
+        canvas.translate(offsetX * scaleFraction,offsetY * scaleFraction)
+        canvas.scale(currentScale, currentScale, width / 2f, height / 2f)
         canvas.drawBitmap(bitmap, originalOffsetX, originalOffsetY, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        return gestureDecoder.onTouchEvent(event)
-    }
-
-    override fun onDown(e: MotionEvent?): Boolean {
+        scaleGestureDetector.onTouchEvent(event)
+        if (!scaleGestureDetector.isInProgress) {
+            gestureDecoder.onTouchEvent(event)
+        }
         return true
+//        return gestureDecoder.onTouchEvent(event)
+//        return scaleGestureDetector.onTouchEvent(event)
     }
 
-    override fun onShowPress(e: MotionEvent?) {
 
+    private fun fixOffset() {
+        offsetX = min(offsetX, ((bitmap.width * bigScale - width) / 2))
+        offsetX = max(offsetX, -((bitmap.width * bigScale - width) / 2))
+        offsetY = min(offsetY, ((bitmap.height * bigScale - height) / 2))
+        offsetY = max(offsetY, -((bitmap.height * bigScale - height) / 2))
     }
 
-    override fun onSingleTapUp(e: MotionEvent?): Boolean {
-        return false
-    }
 
-    override fun onScroll(
-        startEvent: MotionEvent?,
-        currentEvent: MotionEvent?,
-        distanceX: Float, // 这个距离是起点的坐标减去终点的距离的横坐标
-        distanceY: Float // 这个距离是起点的坐标减去终点的距离的纵坐标
-    ): Boolean {
-        if (big) {
-            offsetX -= distanceX
-            offsetX = min(offsetX, ((bitmap.width * bigScale - width) / 2))
-            offsetX = max(offsetX, -((bitmap.width * bigScale - width) / 2))
-            offsetY -= distanceY
-            offsetY = min(offsetY, ((bitmap.height * bigScale - height) / 2))
-            offsetY = max(offsetY, -((bitmap.height * bigScale - height) / 2))
-            invalidate()
-        }
-        return false
-    }
-
-    override fun onLongPress(e: MotionEvent?) {
-    }
-
-    override fun onFling(
-        e1: MotionEvent?,
-        e2: MotionEvent?,
-        velocityX: Float,
-        velocityY: Float
-    ): Boolean {
-        if (big) {
-            scroll.fling(offsetX.toInt(),offsetY.toInt(),velocityX.toInt(),velocityY.toInt(),
-                (-(bitmap.width * bigScale - width) / 2).toInt(),
-                ((bitmap.width * bigScale - width) / 2).toInt(),
-                (-(bitmap.height * bigScale - height) / 2).toInt(),
-                ((bitmap.height * bigScale - height) / 2).toInt())
-            ViewCompat.postOnAnimation(this,this)
-        }
-        return false
-    }
-
-    override fun run() {
-        if (scroll.computeScrollOffset()) {
-            offsetX = scroll.currX.toFloat()
-            offsetY = scroll.currY.toFloat()
-            ViewCompat.postOnAnimation(this,this)
-        }
-    }
-
-    override fun onSingleTapConfirmed(e: MotionEvent?): Boolean {
-        return false
-    }
-
-    override fun onDoubleTap(e: MotionEvent?): Boolean {
-        big = !big
-        if (big) {
-            scaleAnimator.start()
-        } else {
-            scaleAnimator.reverse()
+    inner class GestureListener : GestureDetector.SimpleOnGestureListener(){
+        override fun onDown(e: MotionEvent?): Boolean {
+            return true
         }
 
-        return true
+        override fun onScroll(
+            startEvent: MotionEvent?,
+            currentEvent: MotionEvent?,
+            distanceX: Float, // 这个距离是起点的坐标减去终点的距离的横坐标
+            distanceY: Float // 这个距离是起点的坐标减去终点的距离的纵坐标
+        ): Boolean {
+            if (big) {
+                offsetX -= distanceX
+                offsetY -= distanceY
+                fixOffset()
+                invalidate()
+            }
+            return false
+        }
+
+        override fun onFling(
+            e1: MotionEvent?,
+            e2: MotionEvent?,
+            velocityX: Float,
+            velocityY: Float
+        ): Boolean {
+            if (big) {
+                scroll.fling(offsetX.toInt(),offsetY.toInt(),velocityX.toInt(),velocityY.toInt(),
+                    (-(bitmap.width * bigScale - width) / 2).toInt(),
+                    ((bitmap.width * bigScale - width) / 2).toInt(),
+                    (-(bitmap.height * bigScale - height) / 2).toInt(),
+                    ((bitmap.height * bigScale - height) / 2).toInt())
+                ViewCompat.postOnAnimation(this@TouchScaleImageView,flingRunnable)
+            }
+            return false
+        }
+
+
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            big = !big
+            if (big) {
+                offsetX = (e.x - width / 2f) * (1 - bigScale / smallScale)
+                offsetY = (e.x - height / 2f) * (1 - bigScale / smallScale)
+                fixOffset()
+                scaleAnimator.start()
+            } else {
+                scaleAnimator.reverse()
+            }
+
+            return true
+        }
     }
 
-    override fun onDoubleTapEvent(e: MotionEvent?): Boolean {
-        return false
+    inner class ScaleGestureListener : ScaleGestureDetector.OnScaleGestureListener{
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            // 返回值返回true的时候表示detector.scaleFactor是这一次和上一次的比值
+            // 返回false的时候表示detector.scaleFactor是这一次和第一次的比值
+            val tempScale = currentScale * detector.scaleFactor
+            if (tempScale < smallScale || tempScale > bigScale) {
+                return false
+            } else {
+                currentScale *= detector.scaleFactor
+                return true
+            }
+//            currentScale.coerceAtLeast(smallScale).coerceAtMost(bigScale)
+//            return true
+        }
+
+        override fun onScaleBegin(detector: ScaleGestureDetector): Boolean {
+            // 初始值
+            offsetX = (detector.focusX - width / 2f) * (1 - bigScale / smallScale)
+            offsetY = (detector.focusY - height / 2f) * (1 - bigScale / smallScale)
+            return true
+        }
+
+        override fun onScaleEnd(detector: ScaleGestureDetector?) {
+
+        }
     }
 
+    inner class FlingRunnable : Runnable{
+        override fun run() {
+            if (scroll.computeScrollOffset()) {
+                offsetX = scroll.currX.toFloat()
+                offsetY = scroll.currY.toFloat()
+                ViewCompat.postOnAnimation(this@TouchScaleImageView,this)
+            }
+        }
+    }
 
 }
